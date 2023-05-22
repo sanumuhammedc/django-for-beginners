fae2bdb9e5c4d8011a73b691c5530202acef521b
sanumuhammedc <sanumuhammed.c@gmail.com>
2023-05-22 16:39:22 +0530

    addes shop project

---
+++ shop/products/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    stock = models.IntegerField()
+    image_url = models.CharField(max_length=2083)
+
+class Offer(models.Model):
+    image = models.CharField(max_length=2083)
